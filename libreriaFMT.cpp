619e131ea12c9cd61fe7f947cd4698fc5b1ff0ca
Johan Posada <64292875+johanP051@users.noreply.github.com>
2024-11-06 21:20:09 -0500

    Explicación de FMT

--- libreriaFMT.cpp
+++ libreriaFMT.cpp
@@ -10,13 +10,8 @@ using namespace std;
 int main(){
     fmt::print("Hola fmt\n");
 
-    int n1;
-    cout << "\nDigite el número 1: ";
-    cin >> n1;
-
-    int n2;
-    cout << "Digite el número 2: ";
-    cin >> n2;
+    int n1 = 1;
+    int n2 = 2;
 
     // Imprimiendo con fmt
     fmt::print("\nEl número 1 es: {}\nEl número 2 es: {}\n", n1, n2);
@@ -26,22 +21,37 @@ int main(){
     cout << "El número 2 es: " << n2 << endl;
     */
 
-   //Imprimiendo con color
-    fmt::print(fg(fmt::color::red) | bg(fmt::color::light_gray) | fmt::emphasis::underline , "\nJugando con el orden:\n");
-
-   //Usando los índices de los argumentos/varibles para imprimir en diferente orden
+    //Usando los índices de los argumentos/varibles para imprimir en diferente orden
     fmt::print("\nEl número 2 es: {1}\nEl número 1 es: {0}\nY el tercer número es: {1}\n", n1, n2);
+
+   //Usando fmt::format, para almacenar una cadena con formato en una variable String
+    string cadena = "2 + 2 es = 4";
+    string cadena_formateada = fmt::format("\nEsta es mi cadena formateada: {}\n", cadena);
+    cout << cadena_formateada;
+
+   //Imprimiendo con color
+    // fmt::print(bg(fmt::color::red), "\n\nProbando cadenas con color de fondo");
+
+    fmt::print(fg(fmt::color::red) 
+                | bg(fmt::color::light_gray) 
+                | fmt::emphasis::underline , "\nC++ es bastante bueno");
+
 
     //Imprimiendo un vector
     int numeros[] = {0 ,1, 2, 3};
-    fmt::print("\nEl vector de numeros es {}\n", numeros);
+    fmt::print("\n\nEl vector de numeros es {}\n", numeros);
     
+    //Usando fmt::join
+    cout << "\nUsando fmt::join para imprimir un vector:";
+    fmt::print("\nEl vector de numeros es [{}]\n", fmt::join(numeros, "; "));
+    // fmt::print("\nEl vector de numeros es {{{}}}\n", fmt::join(numeros, ", "));
+
     // Esto no se puede hacer con cout a menos que se use un bucle for
     // cout << "Arreglo: " << numeros;
 
-    fmt::print("{:_<16}\n", "Texto Izquierda"); //"Texto Izquierda" contiene 15 caracteres, en este caso se ajusta un campo de 16 espacios para ajustar el texto
-    fmt::print("{:_>14}\n", "Texto Derecha"); // "Texto Derecha" contiene 13 caracteres, ... 14 espacios para ajustar el texto
-    fmt::print("{:_^16}\n", "Texto centrado"); // "Texto centrado" contiene 14 caracteres, ...  16 espacios para ajustar el texto
+    fmt::print("{:_<30}\n", "Texto Izquierda"); //"Texto Izquierda" contiene 15 caracteres, en este caso se ajusta un campo de 16 espacios para ajustar el texto
+    fmt::print("{:_>30}\n", "Texto Derecha"); // "Texto Derecha" contiene 13 caracteres, ... 14 espacios para ajustar el texto
+    fmt::print("{:_^30}\n", "Texto centrado"); // "Texto centrado" contiene 14 caracteres, ...  16 espacios para ajustar el texto
 
     return 0;
 }
